502754c0848105e407fec1c90f73fdd908e34f8d
EshanDevStudio <eshan.devstudio@gmail.com>
2025-09-21 23:52:47 +0530

    Added home page HTML structure

---
+++ src/pages/HomeTest.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const HomeTest = () => {
+
+    return (
+        <div className="homeTest">
+            <div className="homeTestBanner" >
+                <div className="homeTestBannerImgCont"></div>
+                <div className="homeTestBannerTxtCont">
+                    <p className="homeTstBannTxtCntHdTxt" >Elevate Your Style</p>
+                    <p className="homeTstBannTxtCntChTxt"><span>with</span> our new collection</p>
+                </div>
+            </div>
+            <div className="homeTestProNavCont">
+                <div className="homeTestProNavContCont">
+                    <div className="hmTstProNavCtWomenNav">
+                        <p>Women's Collection</p>
+                        <span>look our new<br />collection</span>
+                    </div>
+                    <div className="hmTstProNavCtMenNav">
+                        <p>Men's Collection</p>
+                        <span>the best of global brands,<br />at your door step!</span>
+                    </div>
+                </div>
+                <div className="hmTstProNavCntHdCnt">
+                    <p>only</p>
+                    <p className="hmTstProNavCntHdCntSpPra" >the best</p>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default HomeTest
